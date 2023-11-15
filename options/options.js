a090bb4c84c3f0934d444b6d4bb77a070b414ad0
Crowdbotics <team@crowdbotics.com>
2023-11-15 16:44:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pine-straw-app-44570",
+  url: "https://pine-straw-app-44570.botics.co",
+  api: "https://pine-straw-app-44570.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
